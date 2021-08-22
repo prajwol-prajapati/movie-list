6f8af8de5ec3d3d4b8a98f05d372bdf6c6eebe5b
Prajwol Prajapati <prajwolprajapati123@gmail.com>
2021-08-22 17:09:20 +0545

    Initial react setup with necessary package for react with typescript

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = (): JSX.Element => {
+  return <>
+    App containerxcxc
+  </>
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  target: "web",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+    }),
+  ],
+};
