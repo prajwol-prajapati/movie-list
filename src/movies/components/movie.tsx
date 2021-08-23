6b95ef21a3524145a81983c0345668b484b64130
Prajwol Prajapati <prajwolprajapati123@gmail.com>
2021-08-23 07:40:30 +0545

    add acollapsible accordion for movie description

--- src/movies/components/movie.tsx
+++ src/movies/components/movie.tsx
@@ -1,13 +1,15 @@
-import { MovieInterface } from "movies/movies.type"
+import React, { useState } from 'react';
 
-import React from 'react';
+import { MovieInterface } from "movies/movies.type";
 
 interface MoviePropsInterface {
   movieInfo?: MovieInterface
 }
 
 const Movie: React.FC<MoviePropsInterface> = ({ movieInfo }): JSX.Element => {
-  return <div className="movie-container">
+  const [expanded, setExpanded] = useState(false);
+
+  return <div className={`movie-container ${expanded ? 'expanded' : ''}`} onClick={() => setExpanded(!expanded)}>
     <div className="movie-poster-container">
       <img src={movieInfo?.poster} alt={movieInfo?.title} className="movie-poster" />
     </div>
@@ -15,6 +17,11 @@ const Movie: React.FC<MoviePropsInterface> = ({ movieInfo }): JSX.Element => {
       <span className="movie-title">{movieInfo?.title}</span>
       <span className="movie-genre">{movieInfo?.genre}</span>
       <span className="movie-director">{movieInfo?.director.name}</span>
+    </div>
+    <div className="break"></div>
+    <div className="movie-description-container">
+      <span className="movie-description">{movieInfo?.description}</span>
+      <button className="btn-primary add-to-favorite">ADD TO FAVORITE</button>
     </div>
   </div>
 }
